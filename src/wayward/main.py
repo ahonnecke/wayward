a371c18c3564c8edd9c335e021235c412633aecf
Ashton Honnecke <ashton@pixelstub.com>
2023-12-12 13:27:10 -0700

    Image moving is working.
    Bin moving is working.
    cdlc not tested

--- src/wayward/main.py
+++ src/wayward/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import logging
 import os
-import shutil
+import re
+import subprocess
 import time
-from functools import cached_property
 from pathlib import Path
 
 from devtools import debug
@@ -35,34 +35,93 @@ class Watcher:
 class Handler(FileSystemEventHandler):
     def __init__(self, onchange):
         self.onchange = onchange
+        self.BUILDSPACE = Path("/home/ahonnecke/cdlc_buildspace")
 
     def on_any_event(self, event):
         if event.is_directory:
             return None
 
-        elif event.event_type == "created":
+        elif event.event_type == "created" or event.event_type == "modified":
             # Take any action here when a file is first created.
-            print(f"Received created event - {event.src_path}.")
+            print(f"Received created or modified - {event.src_path}.")
             return self.handle_created(event)
 
-        elif event.event_type == "modified":
-            # Taken any action here when a file is modified.
-            print(f"Received modified event - {event.src_path}.")
+        def sanitize_file(self, current):
+            new = re.sub(r"[^\w_. -]", "_", current).replace(" ", "_")
+            if new != current:
+                print(f"{current} => {new}")
+                os.rename(current, new)
+
+        def sanitize_psarcs_in_dir(self, dirpath):
+            print(f"Sanitizing filenames in {dirpath}")
+            for file in dirpath.glob("*.psarc*"):
+                self.sanitize_file(file.name)
+
+    def remote_move_cdlc(self, event):
+        REMOTE_DEST = Path("ahonnecke@rocksmithy:/Users/ahonnecke/dlc/")
+        BACKUP_DEST = Path("/home/ahonnecke/cldc/shipped/")
+
+        for filename in os.listdir(self.BUILDSPACE):
+            result = subprocess.run(
+                [
+                    "scp",
+                    filename,
+                    f"{REMOTE_DEST}/{filename}",
+                    "&&",
+                    "mv",
+                    f"{self.BUILDSPACE}/{filename}",
+                    BACKUP_DEST,
+                ],
+                stdout=subprocess.PIPE,
+            )
+            print(result.stdout)
+
+    def wait_for_file(self, file_path):
+        # Wait for file to stabilized
+        historicalSize = -1
+        while historicalSize != os.path.getsize(file_path):
+            historicalSize = os.path.getsize(file_path)
+            time.sleep(1)
+        return True
+
+    def handle_psarc(self, event):
+        file_path = Path(event.src_path)
+
+        PYROCKSMITH = Path("/home/ahonnecke/.pyenv/shims/pyrocksmith")
+
+        os.rename(event.src_path, f"{self.BUILDSPACE}/{file_path.name}")
+        self.sanitize_psarcs_in_dir(self.BUILDSPACE)
+        result = subprocess.run(
+            [PYROCKSMITH, "--convert", f"{self.BUILDSPACE}/{file_path.name}"],
+            stdout=subprocess.PIPE,
+        )
+        print(result.stdout)
+        self.remote_move(event)
 
     def handle_created(self, event):
-        debug(event)
-
         file_path = Path(event.src_path)
-        print(file_path.suffix)
+        if not file_path.is_file():
+            # event was deletion or move
+            return
+
+        self.wait_for_file(file_path)
+
+        # Wait for file to stabilize
+        historicalSize = -1
+        while historicalSize != os.path.getsize(file_path):
+            historicalSize = os.path.getsize(file_path)
+            time.sleep(1)
+
+        print("File file_path has stabilized")
+
+        # TODO: make the file handler dynamic
+        # ie. return self.handle_{file_path.suffix}
         if file_path.suffix == ".bin":
-            print("found bin file")
-            os.rename(event.src_path, f"/home/ahonnecke/qmk/{file_path.name}")
-        elif file_path.suffix == ".cdlc":
-            os.system("convert_rocksmith.sh")
+            os.rename(file_path, f"/home/ahonnecke/qmk/{file_path.name}")
+        elif file_path.suffix == ".psarc":
+            self.handle_psarc(event)
         elif file_path.suffix in [".png", ".jpg", ".jpeg", ".tiff", ".bmp", ".gif"]:
-            os.rename(
-                event.src_path, f"/home/ahonnecke/Downloads/images/{file_path.name}"
-            )
+            os.rename(file_path, f"/home/ahonnecke/Downloads/images/{file_path.name}")
 
 
 class FileSorter:
